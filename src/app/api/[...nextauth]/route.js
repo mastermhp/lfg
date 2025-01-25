e5c4ab2f8c7ca5dd59c9059c931d52e9b025729d
MEHEDI HASAN PARASH <112463517+mastermhp@users.noreply.github.com>
2025-01-25 16:29:00 +0600

    deployment

--- src/app/api/[...nextauth]/route.js
+++ src/app/api/[...nextauth]/route.js
@@ -1,10 +1,10 @@
-import { authMiddleware } from "@clerk/nextjs"
+// import { authMiddleware } from "@clerk/nextjs"
 
-export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
-})
+// export default authMiddleware({
+//   publicRoutes: ["/", "/api/webhook"],
+// })
 
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// }
 

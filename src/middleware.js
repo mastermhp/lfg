73ea4571e01514cad96b594a0cac255909fce4dc
MEHEDI HASAN PARASH <112463517+mastermhp@users.noreply.github.com>
2025-02-13 17:30:47 +0600

    admin access

--- src/middleware.js
+++ src/middleware.js
@@ -6,7 +6,7 @@ export default clerkMiddleware(async (auth, req) => {
   // Restrict admin routes to specific email addresses
   if (isProtectedRoute(req)) {
     await auth.protect((user) => {
-      if (user?.primaryEmailAddress?.emailAddress !== "parashmehedihasan@gmail.com") {
+      if (user?.primaryEmailAddress?.emailAddress !== "noboatwork@gmail.com") {
         // Send a 403 Forbidden response if the user is not the admin
         return new Response('Forbidden', { status: 403 });
       }
